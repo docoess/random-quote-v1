18941d6e3abd5cc042aae362bd8bd7124cdafea1
Donald Roessler <doc.oess@gmail.com>
2016-06-16 15:06:36 -0700

    Add more main code
    Fleshed out functions and added base functionality plus several extra
    effort functions.

--- js/quotes.js
+++ js/quotes.js
@@ -15,50 +15,56 @@ Each entry has:
 var quoteList = [
 	{
 		quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
-		source: "Chinese Proverb"
+		source: "Chinese Proverb",
+		quoteID: 0
 	},
 	{
 		quote: "The best revenge is massive success.",
-		source:"Frank Sinatra"
+		source:"Frank Sinatra",
+		quoteID: 1
 	},
 	{
 		quote: "Whether you think you can or you think you can’t, you’re right.",
 		source: "Henry Ford",
 		citation: "Reader's Digest",
-		year: 1947
+		year: 1947,
+		quoteID: 2
 	},
 	{
 		quote: "The only person you are destined to become is the person you decide to be.",
-		source: "Ralph Waldo Emerson"
+		source: "Ralph Waldo Emerson",
+		quoteID: 3
 	},
 	{
 		quote: "We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light.",
-		source: "Plato"
+		source: "Plato",
+		quoteID: 4
 	},
 	{
 		quote: "Start where you are. Use what you have.  Do what you can.",
 		source: "Arthur Ashe",
 		citation: "Days of Grace",
-		year: 1993
+		year: 1993,
+		quoteID: 5
 	},
 	{
 		quote: "Fall seven times and stand up eight.",
-		source: "Japanese Proverb"
+		source: "Japanese Proverb",
+		quoteID: 6
 	},
 	{
 		quote: "When I let go of what I am, I become what I might be.",
-		source: "Lao Tzu"
+		source: "Lao Tzu",
+		quoteID: 7
 	},
 	{
-		quote: ,
-		source: ,
-		citation: ,
-		year: 
+		quote: "Test quote 9",
+		source: "me",
+		quoteID: 8
 	},
 	{
-		quote: ,
-		source: ,
-		citation: ,
-		year: 
+		quote: "Quote Test number 10",
+		source: "Also me",
+		quoteID: 9
 	},
 ];
--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-var message = '';
-var quote;
+var selectedQuote;
+var quotesUsed = [];
 
+// Function to grab random number from 0 to 9, which matches the index options for quoteList
+function getRandomNum() {
+	var rand = Math.floor(Math.random() * 10);
+	return rand;
+}
+
+// Function to grab a random quoteID, check if it already has been used and logged, and then return a non-used quote
 function getRandomQuote() {
-	
+	var quoteID = getRandomNum();
+	var quote;
+	while (quotesUsed.indexOf(quoteID) !== -1) {
+		if (quotesUsed.length === 10) {
+			quotesUsed = [];	
+			break;
+		} else {
+			quoteID = getRandomNum();
+		}
+	}
+	quote = quoteList[quoteID];
+	quotesUsed.push(quoteID);
+	return quote;
 }
 
 // Function to grab a value from 0-255 for use in generating a new background color
@@ -15,8 +34,21 @@ function getNewRGBColor() {
 	document.body.style.backgroundColor = 'rgb(' + getNew256() + ',' + getNew256() + ',' + getNew256() + ')';
 }
 
+// Function for modifying the displayed quote, as well as calling getNewRGBColor() to change the color when quotes are changed
 function printQuote() {
+	var quote = getRandomQuote();
+	var message = '';
+	
 	getNewRGBColor();
+	message += '<p class="quote">' + quote.quote + '</p>';
+	message += '<p class="source">' + quote.source + '</p>';
+	if (quote.citation !== undefined) {
+		message += '<span class="citation">' + quote.citation + '</span>';
+	}
+	if (quote.year !== undefined) {
+		message += '<span class="year">' + quote.year + '</span>';
+	}
+	document.getElementById('quote-box').innerHTML = message;
 }
 
 
